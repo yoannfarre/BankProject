1323c599bc6cb1f82e2135605201380c0c43984c
Yoann <yoann.farre@gmail.com>
2017-05-30 23:11:00 +0200

    Account

--- BankProject/src/bankproject/entities/Account.java
+++ BankProject/src/bankproject/entities/Account.java
@@ -1,9 +1,53 @@
 package bankproject.entities;
 
+import bankproject.enumerations.CountryEnum;
+
 public class Account {
-	
+
 	String number;
 	Customer customer;
-	
+
+//	public static void main(String[] args) throws SQLException {
+//
+//		String str = buildNumber(CountryEnum.BRITAIN);
+//		System.out.println(str);
+//	}
+
+	public static String buildNumber(CountryEnum country) {
+
+		StringBuilder sb = new StringBuilder();
+		int digit, i;
+
+		switch (country) {
+
+		case SPAIN:
+			sb.append("ES");
+			break;
+		case FRANCE:
+			sb.append("FR");
+			break;
+		case BRITAIN:
+			sb.append("GB");
+			break;
+		case GERMANY:
+			sb.append("DE");
+			break;
+		case BELGIUM:
+			sb.append("BE");
+			break;
+		case NEDERLANDS:
+			sb.append("NL");
+			break;
+		}
+
+		for (i = 1; i < 7; i++) {
+
+			digit = (int) (Math.random() * 10);
+			sb.append(digit);
+
+		}
+
+		return sb.toString();
+	}
 
 }
